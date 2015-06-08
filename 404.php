76847e2175ad7ce2a97a6907978b18bc5d359a7b
Neil Meskauskis <meskauskis@gmail.com>
2015-06-07 20:10:18 -0400

    Updated 404 page.

--- 404.php
+++ 404.php
@@ -1,3 +1,54 @@
-<section class="site-content clear-fix">
-	<h3 class="not-found">Page Not Found!</h3>
-</section>
+ <!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="Neil Meskauskis Portfolio: UX, Web Developer / Designer, living in NYC." />
+  <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0" />
+  <title data-ng-bind="browserTitle">Neil Meskauskis Portfolio | 404</title>
+
+  <style>
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+    }
+    body {
+      background: transparent url('images/jp-screen-pattern.png') center center repeat;
+    }
+    .main-image {
+      display: block;
+      margin: .8em 0 0 .8em;
+    }
+    .prompt {
+      position: absolute;
+      left: 300px;
+      top: 34px;
+      background-color: #aaaaa8;
+      border: 1px solid #111;
+    }
+    .prompt-inner {
+      border: 2px solid #ccc;
+    }
+    .prompt a {
+      display: block;
+      color: #000;
+      font-family: Terminal, sans-serif;
+      font-size: 14px;
+      text-decoration: none;
+      padding: .6em .8em;
+    }
+    .prompt a:hover {
+      color: #555;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="prompt">
+    <div class="prompt-inner">
+      <a href="/">Return to main site</a>
+    </div>
+  </div>
+  <img class="main-image" src="images/jp-screen.png"/>
+</body>
+</html>
