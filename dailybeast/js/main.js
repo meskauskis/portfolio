0e2ee521c2664d73546dde685157c72ecb8733cf
Neil Meskauskis <meskauskis@gmail.com>
2015-07-03 14:22:31 -0400

    Daily Beast test.

---
+++ dailybeast/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+  var articlesUrl = 'data/articles.json';
+  console.log(articlesUrl);
+
+  var source = document.querySelector('#articleTemplate').innerHTML,
+      template = Handlebars.compile(source),
+      target = document.querySelector('#articlePlaceholder');
+
+  var data = {
+    names: [
+      { "name": "foo" },
+      { "name": "bar" },
+      { "name": "chew" }
+    ],
+    candy: [
+      { brand: "M&M" },
+      { brand: "Snickers" }
+    ]
+  };
+
+
+  $.ajax({
+    url: articlesUrl,
+    dataType: 'json',
+  })
+  .done(function(data) {
+    console.log(data);
+  target.innerHTML = template(data);
+  });
+
+
+
+});
