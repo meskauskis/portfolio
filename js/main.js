7a64d6ea8fed50a5ffb11d3c459b1e4247ca1a45
Neil Meskauskis <meskauskis@gmail.com>
2015-08-06 22:53:34 -0400

    Added schema data. Set page open on default.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $(function() {
-  var $body = $('body'),
-			$pageNav = $('.page-nav'),
-		  $pageSide = $('.page-side');
-
 	// creates html5 elements, it's a fix for IE
 	document.createElement('article');
 	document.createElement('aside');
@@ -14,37 +10,4 @@ $(function() {
 	document.createElement('nav');
 	document.createElement('section');
 	document.createElement('video');
-
-	/*
-	// toggle menu based off of cookie settings
-	if (getCookie('navStatus') === 'open') {
-		$body.removeClass('nav-closed');
-	}
-
-	// set selected nav item (variable set in head)
-	if (navSelected != '') {
-		$('.' + navSelected).addClass('selected');
-	}
-
-	// cookie to track nav bar state
-	function setCookie(cookieName, value, suggestedDays) {
-		var exdate = new Date();
-		exdate.setDate(exdate.getDate() + suggestedDays);
-		
-		var cookieValue = escape(value) + ((suggestedDays === null) ? "" : "; expires=" + exdate.toUTCString());
-		document.cookie = cookieName + "=" + cookieValue;
-	}
-
-	// click event for toggling menu
-	$menuToggle.add($menuToggleMobile).click(function() {
-		var isClosed = $body.hasClass('nav-closed');
-		$body.toggleClass('nav-closed');
-
-		if (isClosed === true) {
-			setCookie('navStatus', 'open', 30);
-		} else {
-			setCookie('navStatus', 'closed', 30);
-		}
-	});
-  */
 });
