701ba8a0d35a4a190618b381de92e83e6ea5c144
Neil Meskauskis <meskauskis@gmail.com>
2015-09-22 16:23:11 -0700

    Removed console logs.

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -50,11 +50,9 @@ app
 
   // If the results have already been loaded.
   if ($rootScope.jsonSectionItems) {
-    console.log('good1');
     $scope.setPageElements($scope, $rootScope.jsonSectionItems, section);
   }
   else {
-    console.log('bad1');
     $http.get('data/sections.json')
     .success(function(data, status, headers, config) {
       $rootScope.jsonSectionItems = data[0];
@@ -74,11 +72,9 @@ app
 
   // If the results have already been loaded.
   if ($rootScope.jsonExamplesItems) {
-    console.log('good2');
     $scope.setPageElements($scope, $rootScope.jsonExamplesItems, section);
   }
   else {
-    console.log('bad2');
     $http.get('data/examples.json')
     .success(function(data, status, headers, config) {
       $rootScope.jsonExamplesItems = data[0];
