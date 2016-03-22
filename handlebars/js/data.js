a844f590f783d2c03e28d45d646ce27fedb02126
meskauskis <meskauskis@gmail.com>
2016-03-22 00:03:04 -0400

    Fixed ID article error quotes.

---
+++ handlebars/js/data.js
@@ -0,0 +1,19 @@
+$(function() {
+  var source = document.getElementById("myTemplate").innerHTML;
+  var template = Handlebars.compile(source);
+  var data = {
+    names: [
+      { "name": "foo" },
+      { "name": "bar" },
+      { "name": "chew" }
+    ],
+    candy: [
+	  	{ brand: "M&M" },
+	  	{ brand: "Snickers" }
+    ]
+  };
+	Handlebars.registerHelper("Max", function(A, B){
+    return (A > B) ? A : B;
+	});
+  document.getElementById("placeholder").innerHTML = template(data);
+});
