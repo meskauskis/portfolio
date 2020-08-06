f290c60fb3949de98027659322633ab71d1212fa
meskauskis <meskauskis@gmail.com>
2020-08-05 21:54:12 -0500

    Setup dev build env.

--- symfony_project/src_js/item.js
+++ symfony_project/src_js/item.js
@@ -1,30 +1,34 @@
 import React, { useState } from 'react';
- 
+
 const App = () => {
-  const [greeting, setGreeting] = useState(
-    'Hello Component:'
-  );
- 
-  const handleChange = event => setGreeting(event.target.value);
- 
-  return (
-    <div>
-      <Headline headline={greeting} />
- 
-      <Input value={greeting} onChangeInput={handleChange}>
-        Set Greeting:
-      </Input>
-    </div>
-  );
+    const [greeting, setGreeting] = useState(
+        'Hello Component:'
+    );
+
+    const handleChange = event => setGreeting(event.target.value);
+
+    return (
+        <>
+            <Headline headline={greeting} />
+     
+            <Input value={greeting} onChangeInput={handleChange}>
+                Set Greeting:
+            </Input>
+        </>
+    );
 };
- 
-const Headline = ({ headline }) => <h1>{headline}</h1>;
- 
+
+const Headline = ({ headline }) => {
+    return(
+        <h4>{headline}</h4>
+    )
+};
+
 const Input = ({ value, onChangeInput, children }) => (
-  <label>
-    {children}
-    <input type="text" value={value} onChange={onChangeInput} />
-  </label>
+    <label>
+        {children}
+        <input type="text" value={value} onChange={onChangeInput} />
+    </label>
 );
- 
+
 export default App;
